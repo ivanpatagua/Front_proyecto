70c0acdc093911eec5cac1e963ae00b5410377ce
Carlos Iván Patagua Ibieta <ivanpatagua@gmail.com>
2023-03-24 05:55:17 -0300

    front terminado, faltan detalles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialesComponent } from './components/sociales/sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
